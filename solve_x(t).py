8de796d1f0c474de0bca0dda0c148dd122aedee5
Daniel Beckford <danielbeckford@Aphrodite.local>
2026-06-08 15:23:04 -0400

    Further corrections

--- solve_x(t).py
+++ solve_x(t).py
@@ -14,7 +14,7 @@ class PNexapansion_x:
         self.nu = (2*M)/M**2            #assume equal masses for now
 
     def setPowers(self, p):     #p is a a real number that gets interated into powers of x
-        if isinstance(p, np.ndarray):
+        if isinstance(p, tuple):
             self.p = np.array(p)
         else:
             self.p = np.zeros(p+1)  #array of powers of x
@@ -37,26 +37,25 @@ class PNexapansion_x:
 
         return self.consts
     
-    def differentiate(self): #this is the differentiation
-        if len(self.p) <= 1:  
-            return np.array([0]), np.array([0])
-        
-        self.new_p = self.p[1:] - 1
+    def differentiate(self): #this is the differentiation  
+        self.new_p = self.p[1:] - 1 #discard the first element, so i always need to input a zeroth power, even if the coefficient is 0
         self.new_consts = self.consts[1:] * self.p[1:]
 
         return self.new_p, self.new_consts
     
     def get_Eq(self):       #combines arrays and reshapes equation into n x 2 matrix
-        y = self.p, self.consts
-        return np.array(y)
+        y = np.array([self.p, self.consts], dtype=np.longdouble)
+
+        return y
     
 def eval_function(powers_const, x):
     y = 0
+    y_big = np.longdouble(y)
 
     for j in range(len(powers_const[0])):
-        y += powers_const[1,j]*(x**powers_const[0,j])
+        y_big += powers_const[1,j]*(x**powers_const[0,j])
 
-    return y
+    return y_big
 
 #from down on its setting parameters which gets super SUPER messy
 
@@ -66,14 +65,14 @@ F = PNexapansion_x(M=1, r=1)
 #initialize constants from PNpedia
 E_1 = 1
 E_2 = -(3/4)-(E.nu/12)
-E_3 = -(27/8)+(19*E.nu/8)-(E.nu**2/24)
+E_3 = -(27/8)+(19*E.nu/8)-((E.nu**2)/24)
 
 #build energy equation
-E.setPowers(int(3))
-E.setConstants((0,E_1, E_2, E_3), alpha = (-0.5)*E.M*E.nu*(E.c**2))
+E.setPowers((0,1,2,3))
+E.setConstants((0,-E_1, -E_2, -E_3))
 
 
-powers_F = np.array((5,6,5.5,7,7.5))
+powers_F = (5,6,6.5,7,7.5)
 F_1 = 1
 F_2 = -(1247/336)-((35*F.nu)/12)
 F_3 = 4*np.pi
@@ -81,7 +80,7 @@ F_4 = -(44711/9072)+(9271*F.nu/504)-(65*(F.nu**2)/18)
 F_5 = -(8191*np.pi/672)-(583*np.pi*F.nu/24)
 
 F.setPowers(powers_F)
-F.setConstants((F_1,F_2,F_3,F_4,F_5), alpha = (32*F.c**5*F.nu**2)/5*F.G)
+F.setConstants((F_1,F_2,F_3,F_4,F_5))
 
 
 #4x2 matrices of constants and powers
@@ -90,18 +89,25 @@ dE_dx = np.array(E.differentiate())
 F_x = F.get_Eq()
 
 #solve ODE
-def ode(t, u):
-    x = u[0]
+def ode(t, x):
     y = eval_function(F_x, x)/eval_function(dE_dx, x)
     return y
 
-x0 = [0.1]
-t_span = (0, 50)  
-solution = solve_ivp(ode, t_span, x0, method='RK45', t_eval=np.linspace(0, 50, 100))
+x0 = [0.2]
+t_span = (0, 60)  
+solution = solve_ivp(ode, t_span, x0, method='RK45', t_eval=np.linspace(0, 60, 50))
+
+x = np.linspace(0, 5, 10)
+A = eval_function(dE_dx, x)
+B = eval_function(F_x, x)
+C = eval_function(E_x, x)
 
 times = solution.t
 values = solution.y[0]
 
 plt.plot(times, values, label='x(t)')
+#plt.plot(x, A)
+#plt.plot(x, C)
+
 plt.legend()
 plt.show()
