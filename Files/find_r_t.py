7139f5277552b2f87c360ae29a3e0c2a3c91952b
Daniel Beckford <danielbeckford@Aphrodite.local>
2026-06-14 01:11:28 -0400

    Using gamma PN-expansion

--- Files/find_r_t.py
+++ Files/find_r_t.py
@@ -3,46 +3,51 @@ import matplotlib.pyplot as plt
 
 from solve_x_t import PNexapansion_x
 from solve_x_t import m1, m2, r
-from solve_x_t import times, x_vals, eval_function, E
+from solve_x_t import times, x_vals, eval_function, E, E_x
 
 from find_h_t import PSI_x
 
 E_binding = eval_function(E.get_Eq(), x_vals)
 
-#find r
-def find_r(k, K):
+#find r, K = -binding_energy
+def find_r_using_K(E):
     r = 0
     r_big = np.longdouble(r)
+    K_energy = -(eval_function(E_x, x_vals))
 
-    r_big = (-1/(2*k)) * E.G * E.m1 * E.m2
+    r_big = (-1/(4*K_energy)) * E.G * E.m1 * E.m2
     return r_big
 
-#kinetic energy is equal to the negative of the total conserved energy
-K = PNexapansion_x(m1, m2, r)
+#combined_r = find_r_using_K(E)
 
-K_0 = 1
-K_1 = (3/2) + (K.nu/4)
-K_2 = (27/8) + ((19*K.nu)/8) + ((K.nu**2)/24)
+#using gamma PN-parameters
+Gamma = PNexapansion_x(m1, m2, r)
+Gamma.setPowers(3)
 
-#2nd order kinetic energy
-K.setPowers(3)
-K.setConstants((0, K_0, K_1, K_2), alpha = ((-1/2)*(K.c**2)*(K.M)*(K.nu)))
-K_x = K.get_Eq()
+G_0 = 1
+G_1 = 1 - (Gamma.nu/3)
+G_2 = 1 - (65*Gamma.nu/12)
 
-K_energy = eval_function(K_x, x_vals)
-combined_r = find_r(K_energy, K)
+Gamma.setConstants((0, G_0, G_1, G_2))
+gamma = eval_function(Gamma.get_Eq(), x_vals)
+r_gamma = (1/2)*(Gamma.G * Gamma.M)/(Gamma.c**2 * gamma)
 
-#converting to cartesian coordinates
-x = combined_r * np.cos(PSI_x)
-y = combined_r * np.sin(PSI_x)
+#converting to cartesian coordinates using kinetic energy
+#x = combined_r * np.cos(PSI_x)
+#y = combined_r * np.sin(PSI_x)
 
-x2 = -combined_r * np.cos(PSI_x)
-y2 = -combined_r * np.sin(PSI_x)
+#x2 = -combined_r * np.cos(PSI_x)
+#y2 = -combined_r * np.sin(PSI_x)
 
-print(x)
+#converting to cartesian using Gamma expansion
+x = r_gamma * np.cos(PSI_x)
+y = r_gamma * np.sin(PSI_x)
 
-plt.figure(figsize=(10, 10))
+x2 = -r_gamma * np.cos(PSI_x)
+y2 = -r_gamma * np.sin(PSI_x)
+
 plt.plot(x, y)
 plt.plot(x2, y2)
-#plt.show()
+plt.show()
+
 
