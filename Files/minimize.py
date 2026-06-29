74fa991b8fea1e151a26026009fe92251b5bfd40
Daniel Beckford <danielbeckford@Aphrodite.local>
2026-06-29 16:46:06 -0400

    changes from meeting on June 29

--- Files/minimize.py
+++ Files/minimize.py
@@ -4,11 +4,13 @@ from scipy.integrate import solve_ivp
 from scipy.interpolate import CubicSpline, PchipInterpolator
 from scipy.optimize import minimize
 
-from solve_x_t import PNexapansion_x, E, F, ode, eval_function, pole_event, limit, t_span, step, E_0, E_1, E_2, F_0, F_1, F_2, F_3
-from find_h_t import H_2_2_X, PSI_X, imganinary_exponent, experiment, h_strain_22
+from solve_x_t import PNexapansion_x, E, F, eval_function, pole_event, limit, t_span, step, E_0, E_1, E_2, F_0, F_1, F_2, F_3, x0
+from find_h_t import H_2_2_X, PSI_X, imganinary_exponent, experiment
 
-def f_min(x0, M, nu, target_strain=experiment):
+def f_min(parameters, x0=x0, target_strain=experiment, plot=False):
     #building the PNequations that will change with the minimization algorithm
+    M, nu = parameters
+
     dummy_E = PNexapansion_x()
     dummy_F = PNexapansion_x()
 
@@ -55,20 +57,46 @@ def f_min(x0, M, nu, target_strain=experiment):
 
     # Choose a common grid
     N = max(len(target_strain[0]), len(sol.t))
-    t_common = np.linspace(t_min, t_max, N)
+    t_common = target_strain[0]
 
     # Interpolate both waveforms onto the common grid
-    original_x_interpolated = np.interp(t_common, target_strain[0], target_strain[1])
+    original_strain_interpolated = np.interp(t_common, target_strain[0], target_strain[1])
     solution_dummy_interpolated = np.interp(t_common, sol.t, dummy_h_strain)
 
-    corr = np.correlate(original_x_interpolated, solution_dummy_interpolated, mode='full')
-    lag = np.argmax(corr) - (len(solution_dummy_interpolated)-1)
-    solution_aligned = np.roll(solution_dummy_interpolated, lag)
+    #finding and fixing the lag between the actual waveform and test waveforms
+    # corr = np.correlate(original_strain_interpolated, solution_dummy_interpolated, mode='full')
+    # lag = np.argmax(corr) - (len(solution_dummy_interpolated)-1)
+    # dt = t_common[1] - t_common[0]
+    # t_shifted = t_common + lag * dt
 
-    difference_vector = original_x_interpolated - solution_aligned
+    # solution_aligned = np.interp(t_common, t_shifted, solution_dummy_interpolated, left=0.0, right=0.0)
 
-    return np.linalg.norm(difference_vector)
+    if plot:
+        plt.plot(t_common, original_strain_interpolated, label='target')
+        plt.plot(t_common, solution_dummy_interpolated, label='attempt')
+        plt.legend()
+        plt.show()
 
-x0 = [0.05]
-res = minimize(f_min, x0, method='Nelder-Mead', args=(1, 0.25, experiment), options={'xatol': 1e-8, 'disp': True})
-print(res.x)
+    target_norm = abs(original_strain_interpolated)
+
+    difference_vector = abs(original_strain_interpolated) - abs(solution_dummy_interpolated)
+
+    return np.linalg.norm(difference_vector) / np.linalg.norm(target_norm)
+
+initial = [0.85, 0.2]
+
+# M_values = np.arange(0.85, 1.2, 0.01)
+# f_values = [f_min(M_value, 0.25, x0, experiment, False) for M_value in M_values]
+# print(f_values)
+# plt.plot(M_values, f_values)
+# plt.yscale('log')
+# plt.show()
+
+res = minimize(f_min, initial, method='Nelder-Mead', args=(x0, experiment), options={'xatol': 1e-18, 'disp': True})
+print(res.x, res.fun)
+f_min(res.x, x0, experiment, True)
+
+#Newton-CG
+#Powell
+
+
