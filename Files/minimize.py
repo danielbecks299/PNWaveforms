61889ab5aa3a096d7a566bb592977e01b2117415
Daniel Beckford <danielbeckford@Aphrodite.local>
2026-07-02 14:04:39 -0400

    it kinda works for 3 variables

--- Files/minimize.py
+++ Files/minimize.py
@@ -2,24 +2,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 from scipy.interpolate import CubicSpline, PchipInterpolator
-from scipy.optimize import minimize
+from scipy.optimize import minimize, Bounds
 
 from solve_x_t import PNexapansion_x, E, F, eval_function, pole_event, limit, t_span, step, x0
 from find_h_t import PSI, imganinary_exponent, experiment
 
-def f_min(parameters, x0 = x0, target_strain=experiment, plot=False):
-    #building the PNequations that will change with the minimization algorithm
-    M, nu = parameters
+dummy_E = PNexapansion_x()
+dummy_F = PNexapansion_x()
+dummy_PSI = PNexapansion_x()
+dummy_H21 = PNexapansion_x()
+dummy_H22 = PNexapansion_x()
+dummy_H33 = PNexapansion_x()
 
-    dummy_E = PNexapansion_x()
-    dummy_F = PNexapansion_x()
+def f_min(parameters, target_strain=experiment, plot=False):
+    #building the PNequations that will change with the minimization algorithm
+    M = parameters[0]
+    nu = parameters[1]
+    x0 = [parameters[2]]
 
     E_0 = 1
     E_1 = (-3/4) - (nu/12)
     E_2 = (-27/8) + (19*nu/8) - ((nu**2)/24)
 
-    dummy_E.setM(M)
-    dummy_E.setNu(nu)
     dummy_E.setPowers(E.p)
     dummy_E.setConstants((0, E_0, E_1, E_2), alpha = -(0.5)*(dummy_E.c)**2*(M)*nu)
     dummy_dE_dx = dummy_E.differentiate()
@@ -29,8 +33,6 @@ def f_min(parameters, x0 = x0, target_strain=experiment, plot=False):
     F_2 = 4*np.pi
     F_3 = -(44711/9072) + (9271*nu/504) + (65*(nu**2)/18)
 
-    dummy_F.setM(M)
-    dummy_F.setNu(nu)
     dummy_F.setPowers(F.p)
     dummy_F.setConstants((F_0, F_1, F_2, F_3), alpha = 32*(dummy_F.c**5)*(nu**2)/(5*dummy_F.G)) 
 
@@ -43,7 +45,7 @@ def f_min(parameters, x0 = x0, target_strain=experiment, plot=False):
 
         return y
 
-    sol = solve_ivp(ode, t_span, x0, events=pole_event, method='RK45', t_eval=np.linspace(0, limit, step)) #where are these terms coming from....
+    sol = solve_ivp(ode, t_span, x0, events=pole_event, method='RK45', t_eval=np.linspace(0, limit, step)) 
     solution_dummy = sol.y[0] #just getting the x(t) values
 
     if not sol.success:
@@ -55,13 +57,10 @@ def f_min(parameters, x0 = x0, target_strain=experiment, plot=False):
     PSI_2 = -10*np.pi
     PSI_3 = -32*nu*((-27145/32256)-(15293365/(32514048 * nu)) - (3085*nu/4608)) 
 
-    dummy_PSI = PNexapansion_x()
-    dummy_PSI.setM(M)
-    dummy_PSI.setNu(nu)
     dummy_PSI.setPowers(PSI.p)
     dummy_PSI.setConstants((PSI_0, PSI_1, PSI_2, PSI_3), alpha= -1/(32*nu))
 
-    dummy_PSI_X = PSI.get_Eq()
+    dummy_PSI_X = dummy_PSI.get_Eq()
     dummy_PSI_x = eval_function(dummy_PSI_X, solution_dummy)
 
     dummy_e_PSI_x_1 = np.real(imganinary_exponent(dummy_PSI_x, 1))
@@ -69,7 +68,6 @@ def f_min(parameters, x0 = x0, target_strain=experiment, plot=False):
     dummy_e_PSI_x_3 = np.real(imganinary_exponent(dummy_PSI_x, 3))
 
     #solving for H_2,1
-    dummy_H21 = PNexapansion_x()
     Delta = nu*(1-(4*nu))
 
     if Delta <= 0:
@@ -91,8 +89,6 @@ def f_min(parameters, x0 = x0, target_strain=experiment, plot=False):
     dummy_h_strain_21 =np.real(dummy_e_PSI_x_1 * dummy_H21_x)
 
     #solving for H_2,2
-    dummy_H22 = PNexapansion_x()
-
     H_0 = 1
     H_1 = (-107/42) + ((55*nu)/42)
     H_2 = 2*np.pi
@@ -107,10 +103,6 @@ def f_min(parameters, x0 = x0, target_strain=experiment, plot=False):
     dummy_h_strain_22 =np.real(dummy_e_PSI_x_2 * dummy_H22_x)
 
     #solving for H_3,3
-    dummy_H33 = PNexapansion_x()
-    dummy_H33.setM(M)
-    dummy_H33.setNu(nu)
-
     f = 1.5
     H_3_3_0 = 1
     H_3_3_1 = -4 + (2*nu)
@@ -136,7 +128,6 @@ def f_min(parameters, x0 = x0, target_strain=experiment, plot=False):
         return np.inf
 
     # Choose a common grid
-    N = max(len(target_strain[0]), len(sol.t))
     t_common = target_strain[0]
 
     # Interpolate both waveforms onto the common grid
@@ -157,13 +148,20 @@ def f_min(parameters, x0 = x0, target_strain=experiment, plot=False):
         plt.legend()
         plt.show()
 
-    target_norm = np.abs(original_strain_interpolated)
+    target_norm = original_strain_interpolated
 
-    difference_vector = np.abs(original_strain_interpolated) - np.abs(solution_aligned)
+    difference_vector = original_strain_interpolated - solution_aligned
 
     return np.linalg.norm(difference_vector) / np.linalg.norm(target_norm)
 
-initial = [0.8, 0.15]
+initial = np.array([0.9, 0.2, 0.06])
+
+initial_simplex = np.array([
+    initial,
+    initial + [0.1, 0, 0],
+    initial + [0, 0.1, 0],
+    initial + [0, 0, 0.001],
+])
 
 # M_values = np.arange(0.85, 1.2, 0.01)
 # f_values = [f_min(M_value, 0.25, x0, experiment, False) for M_value in M_values]
@@ -172,9 +170,19 @@ initial = [0.8, 0.15]
 # plt.yscale('log')
 # plt.show()
 
-res = minimize(f_min, initial, method='Nelder-Mead', args=(x0, experiment), options={'xatol': 1e-15, 'disp': True})
+bnds = Bounds([0, 0, 0], [np.inf, 0.25, 0.2])
+
+res = minimize(f_min, initial, method='Nelder-Mead', args=(experiment), bounds = bnds, options={"initial_simplex": initial_simplex, 'xatol': 1e-11, 'fatol': 1e-11, 'maxfev': 10000, 'maxiter': 10000, 'disp': True})
 print(res.x, res.fun)
-f_min(res.x, x0, experiment, True)
+f_min(res.x, experiment, True)
+
+# xs = np.linspace(0.03, 0.13, 50)
+
+# vals = [f_min([1, 0.25, x]) for x in xs
+
+# plt.plot(xs, vals)
+# plt.grid()
+# plt.show()
 
 #Newton-CG
 #Powell
