ff76e940e065367301efeac40a93ee35c3b94f6e
Daniel Beckford <danielbeckford@Aphrodite.local>
2026-07-08 03:34:47 -0400

    made it an exportable function

--- Files/find_r_t.py
+++ Files/find_r_t.py
@@ -18,46 +18,51 @@ def find_r_using_K(E):
     r_big = (-1/(2*K_energy)) * E.G * E.m1 * E.m2
     return r_big
 
-#using gamma PN-parameters
-Gamma = PNexapansion_x(m1, m2)
-Gamma.setPowers(3)
 
-G_0 = 1
-G_1 = 1 - (Gamma.nu/3)
-G_2 = 1 - (65*Gamma.nu/12)
+def get_trajectories(m1, m2, nu):
+    #using gamma PN-parameters
+    Gamma = PNexapansion_x(m1, m2)
+    Gamma.setPowers(3)
+    Gamma.setNu(nu)
 
-Gamma.setConstants((0, G_0, G_1, G_2))
-gamma = eval_function(Gamma.get_Eq(), x_vals)
-r_gamma = (Gamma.G * Gamma.M)/(Gamma.c**2 * gamma) #the separation between the 2 bhs
+    G_0 = 1
+    G_1 = 1 - (Gamma.nu/3)
+    G_2 = 1 - (65*Gamma.nu/12)
 
-#recall, m1r1 + m2r2 = 0
-r1 = (Gamma.m2/Gamma.M)*r_gamma
-r2 = (Gamma.m1/Gamma.M)*r_gamma
+    Gamma.setConstants((0, G_0, G_1, G_2))
+    gamma = eval_function(Gamma.get_Eq(), x_vals)
+    r_gamma = (Gamma.G * Gamma.M)/(Gamma.c**2 * gamma) #the separation between the 2 bhs
 
-
-#converting to cartesian coordinates using kinetic energy
-#combined_r = find_r_using_K(E)
-
-#x = combined_r * np.cos(PSI_x)
-#y = combined_r * np.sin(PSI_x)
-
-#x2 = -combined_r * np.cos(PSI_x)
-#y2 = -combined_r * np.sin(PSI_x)
-
-#converting to cartesian using Gamma expansion
-x = r1 * np.cos(PSI_x)
-y = r1 * np.sin(PSI_x)
-
-x2 = -r2 * np.cos(PSI_x)
-y2 = -r2 * np.sin(PSI_x)
-
-plt.xlabel(r"$x$")
-plt.ylabel(r"$y$")
-
-plt.plot(x, y, label=f'Mass 1 = {Gamma.m1}, $x_0 = {x_vals[0]}$')
-plt.plot(x2, y2, label=f'Mass 2 = {Gamma.m2}')
-plt.legend()
-
-plt.show()
+    #recall, m1r1 + m2r2 = 0
+    r1 = (Gamma.m2/Gamma.M)*r_gamma
+    r2 = (Gamma.m1/Gamma.M)*r_gamma
 
 
+    #converting to cartesian coordinates using kinetic energy
+    #combined_r = find_r_using_K(E)
+
+    #x = combined_r * np.cos(PSI_x)
+    #y = combined_r * np.sin(PSI_x)
+
+    #x2 = -combined_r * np.cos(PSI_x)
+    #y2 = -combined_r * np.sin(PSI_x)
+
+    #converting to cartesian using Gamma expansion
+    x = r1 * np.cos(PSI_x)
+    y = r1 * np.sin(PSI_x)
+
+    x2 = -r2 * np.cos(PSI_x)
+    y2 = -r2 * np.sin(PSI_x)
+
+    plt.xlabel(r"$x$")
+    plt.ylabel(r"$y$")
+
+    plt.plot(x, y, label=f'Mass 1 = {Gamma.m1}, $x_0 = {x_vals[0]}$')
+    plt.plot(x2, y2, label=f'Mass 2 = {Gamma.m2}')
+    plt.legend()
+
+    plt.show()
+
+    return [x,y,x2,y2]
+
+get_trajectories(m1, m2, E.nu)
