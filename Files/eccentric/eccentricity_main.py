52827cd4cd11ab823162b638b20097866b4e3bcf
Daniel Beckford <danielbeckford@Aphrodite.local>
2026-07-17 13:25:57 -0400

    Flipped the sign on F and added waveforms

--- Files/eccentric/eccentricity_main.py
+++ Files/eccentric/eccentricity_main.py
@@ -10,13 +10,12 @@ start_time = time.time()
 
 G, c, m1, m2 = 1, 1, 0.5, 0.5
 M = m1 + m2
-nu = (m1*m2)/(m1+m2)**2 
+nu = (m1*m2)/M**2 
 
 def E_xi(x, i):
     alpha = (-0.5 * (c**2) * M * nu * x)
     E0 = 1
     E1 = (5/4) - (2/i) - (nu/12)
-    E2 = (5/8) + (5/i**2) + ((5-(2*nu))/jnp.sqrt(i)) + ((-5+(nu/3))/i) - ((5*nu)/8) - (nu**2/24)
 
     E = alpha * (E0 + E1*x)
     return E
@@ -62,17 +61,35 @@ def e_t(x, i):
 
 def e_r(x, i):
     e_r0 = 1 - i
-    e_r1 = (3*nu)/2 - (3/4) + i*((5*nu)/ - (15/4))
+    e_r1 = (3*nu)/2 - (3/4) + i*((5*nu)/6 - (15/4))
 
     e_rxi = e_r0 + (e_r1*x)
     return e_rxi
 
 def e_phi_22(x, i):
     e_phi0 = 1 - i
-    e_phi1 = (-3/4) + (5*nu)/2 + i*((nu/6) - (15/8))
+    e_phi1 = (-3/4) + (5*nu)/2 + i*((-nu/6) - (15/8))
 
     e_phi_22xi = e_phi0 + (e_phi1*x)
     return e_phi_22xi
+
+def H22(r, phi, x, i, t):
+    alpha = (4*G*M*nu / c**4) * np.sqrt(np.pi/5) * np.exp(-1j * 2 * phi)
+    dr = np.gradient(r, t)
+    dphi = np.gradient(phi, t)
+
+    H22_0 = (G*M)/r + (r**2 * dphi**2) + (2j * r * dr * dphi) - dr**2
+    H22_1 = (1 / c**2) * (
+        ((G*M/r)**2 * (nu/2 - 5)) 
+        + (G*M*dr**2/r) * (-15/14 - 16*nu/7) 
+        + (dr**4 * (27*nu/14 - 9/14)) 
+        + (r * dr**3 * dphi * ((9j - 27j * nu)/7)) 
+        + (G*M*r*dphi**2 * (11/42 + 26*nu/7)) 
+        + ((dphi*r)**4 * (9/14 - 27*nu/14)) 
+        + (dr * (G*M*dphi * (25j/21 + 45j*nu/7) + (r * dphi)**3 * (9j/7 - 27j*nu/7)))
+    )
+
+    return alpha * (H22_0 + H22_1*x)
             
 def ode_xi(t, y):
     x, i = y
@@ -88,8 +105,9 @@ def ode_xi(t, y):
 
     denom = dEdx*dJdi - dJdx*dEdi
 
-    dxdt = (F*dJdi - dEdi*dJdt) / denom
-    didt = (dEdx*dJdt - F*dJdx) / denom
+    #only flipped the sign on F
+    dxdt = (-F*dJdi - dEdi*dJdt) / denom
+    didt = (dEdx*dJdt + F*dJdx) / denom
 
     return [float(dxdt), float(didt)]
 
@@ -107,7 +125,7 @@ def find_omega(x):
 
 def i_event(t, y):
     x, i = y
-    return i - 1.1
+    return i-1
 
 i_event.terminal = True      # stop integration
 i_event.direction = 1         # only trigger when i increases through 1
@@ -131,40 +149,51 @@ dJ_dx = jit(grad(J_xi, argnums=0))
 dJ_di = jit(grad(J_xi, argnums=1))
 
 start = 0
-limit = 100_000
-step = 200_000_000
+limit = 500_000
+step = 10_000_000
 t_span = (start, limit)  
 
-x0, i0 = 0.05, 0.8 #0.0530009151042, 0.8
+x0, i0 = 0.018, 0.7 #0.036, 0.7
 y0 = [x0, i0]
 
 start_time = time.time()
 
-sol_xi = solve_ivp(ode_xi, t_span, y0, method='RK45', events=[denom_event], rtol=1e-8, atol=1e-10, t_eval=np.linspace(start, limit, step))
+#solve ODE
+sol_xi = solve_ivp(ode_xi, t_span, y0, method='RK45', events=[denom_event, i_event], rtol=1e-8, atol=1e-10, t_eval=np.linspace(start, limit, step))
 t = sol_xi.t
 x = sol_xi.y[0]
 i = sol_xi.y[1]
 
+#find l(t), mean anomaly
 omega = find_omega(x)
 dl_dt = (omega*i)/((3*x + i))
 l = cumulative_trapezoid(dl_dt, t, initial=0.0)
 
-#v_t, phi_t
-
-e_txi = e_t(x, i)
+#use this to find the eccentricity
+e_txi = np.sqrt(e_t(x, i))
 u = invert_kepler(l, e_txi)
 
-r = a_t(x,i) * (1 - e_r(x, i) * np.cos(u))
+#find r(t)
+e_rxi = np.sqrt(e_r(x, i))
+r = a_t(x,i) * (1 - (e_rxi * np.cos(u)))
 
-v_xi = 2 * np.atan2(np.sqrt((1 + e_phi_22(x,i) * np.sin(u/2))), (np.sqrt(1 - e_phi_22(x, i) * np.cos(u/2))))
+#find the eccentric phi and regular phi
+ephi = np.sqrt(e_phi_22(x, i))
+raw_v = np.arctan2(np.sqrt(1 + ephi) * np.sin(u/2), np.sqrt(1 - ephi) * np.cos(u/2))
+v_xi = 2 * np.unwrap(raw_v)
 phi_xi = (1 + (3*x)/i) * v_xi
 
-x_coords = r/2 * np.cos(phi_xi)
-y_coords = r/2 * np.sin(phi_xi)
+#plotting the 2,2 mode
+wave = H22(r, phi_xi, x, i, t)
 
-x_coords2 = -x_coords
-y_coords2 = -y_coords
-plt.rcParams['agg.path.chunksize'] = 20000
+#cartesian coords
+r1 = r * (m2 / M)
+x_coords = r1 * np.cos(phi_xi)
+y_coords = r1 * np.sin(phi_xi)
+
+r2 = r * (m1 / M)
+x_coords2 = -r2 * np.cos(phi_xi)
+y_coords2 = -r2 * np.sin(phi_xi)
 
 plt.plot(t, i, label='iota')
 plt.plot(t, x, label='x')
@@ -181,6 +210,12 @@ plt.show()
 plt.plot(t, r, label='r(t)')
 plt.xlabel(r"$Time, t$")
 plt.ylabel(r"$r$")
+plt.legend(title=f"$m_1 ={m1}, m_2 = {m2}, x_0 = {x[0]}, i_0 = {i[0]}$, 1st Order")
+plt.show()
+
+plt.plot(t, np.real(wave))
+plt.xlabel(r"$Time, t$")
+plt.ylabel(r"$h$")
 plt.legend(title=f"$m_1 ={m1}, m_2 = {m2}, x_0 = {x[0]}, i_0 = {i[0]}$, 1st Order")
 plt.show()
 
